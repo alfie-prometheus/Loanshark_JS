f60661f43df329f1d3133eacb315c5b5ad2c7aca
Alfie Jaiyoon Kim <72483011+alfie-prometheus@users.noreply.github.com>
2024-08-10 14:12:44 -0400

    image file deletions

--- js/site.js
+++ js/site.js
@@ -1,4 +1,11 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello world");
-}
+function formatNumber(input) {
+    // Remove non-numeric characters
+    let number = input.value.replace(/\D/g, '');
+  
+    // Add commas as thousands separators
+    number = number.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  
+    // Update the input value directly
+    input.value = number;
+  }
+  
